073dd2872ebb2768411a5151aa7bcaebe22da344
eloworld-star <testcodylynx@gmail.com>
2022-12-06 11:50:39 +0100

    Update AdvendCalendar-Case6a.py

--- AdvendCalendar-Case6a.py
+++ AdvendCalendar-Case6a.py
@@ -13,18 +13,6 @@ for line in file1:
                 break
             else:
                 seen.append(element)
-
         if len(seen)==len(partToCheck):
             found=True
-
         i+=1
-
-        #if partToCheck[0]==partToCheck[1] or partToCheck[0]==partToCheck[2] or partToCheck[0]==partToCheck[3]:
-        #    found=False
-        #elif partToCheck[1]==partToCheck[2] or partToCheck[1]==partToCheck[3]:
-        #    found=False
-        #elif partToCheck[2]==partToCheck[3]:
-        #    found=False
-        #else:
-        #    found=True
-        #    print("found")
